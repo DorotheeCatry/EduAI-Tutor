467dcd8a039ba8469d566c7c62a4745edb1088c4
Dorothée Catry <catrydorothee@gmail.com>
2025-07-08 14:05:31 +0200

    login working

--- apps/core/views.py
+++ apps/core/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
+
 @login_required
 def homepage(request):
+    return render(request, 'courses/generate.html')
