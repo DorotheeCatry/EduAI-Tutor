3ecc430129f207840e684fde1423432d407192b8
Dorothée Catry <catrydorothee@gmail.com>
2025-07-23 00:52:53 +0200

    fix: chat

--- apps/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'core'
-
-urlpatterns = [
-    path('dashboard/', views.homepage, name='dashboard'),
-]
--- apps/core/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import redirect
-from django.contrib.auth.decorators import login_required
-
-
-@login_required
-def homepage(request):
-    """Redirect to course generator."""
-    return redirect('courses:generator')
