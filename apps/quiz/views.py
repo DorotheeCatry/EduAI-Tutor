42846189b520d2882a11863fe4f67881bb5a2a2e
Dorothée Catry <114245234+DorotheeCatry@users.noreply.github.com>
2025-07-24 10:08:54 +0200

    Corriger l'import manquant de redirect dans quiz/views.py

--- apps/quiz/views.py
+++ apps/quiz/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
